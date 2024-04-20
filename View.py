c92a9ee93c35932b52a3de467fa9a8518ed4cce6
Louise Cacananta <123799928+Ruwweez@users.noreply.github.com>
2024-04-20 14:50:24 +0800

    Create View.py

---
+++ View.py
@@ -0,0 +1,11 @@
+import Model
+
+def login_View():
+    name = input("Name: ")
+    print("\n")
+    password = input("Password: ")
+    return login_Model(name, password):
+print("Login Successful!")
+
+
+login_View()
